0e02e4a63b42e30af3c0529a79d067a06fc78bbb
Francisco Brito <franciscobrito@Franciscos-MBP.lan>
2018-03-04 10:38:00 +0000

    started playing around with ising 2d

---
+++ mc_module/ising.cpp
@@ -0,0 +1,30 @@
+//
+//  ising.cpp
+//  
+//
+//  Created by Francisco Brito on 03/03/2018.
+//
+
+#include <iostream>
+#include <armadillo>
+#include <cstdlib>
+#include <typeinfo>
+#include <cmath>
+#include <random>
+#include <time.h>
+#include <Eigen/Dense>
+
+#define N_SITES 8
+
+using namespace arma;
+using namespace Eigen;
+using namespace std;
+int main()
+{
+    MatrixXd m = MatrixXd::Random(3,3);
+    m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    cout << "m =" << endl << m << endl;
+    VectorXd v(3);
+    v << 1, 2, 3;
+    cout << "m * v =" << endl << m * v << endl;
+}
